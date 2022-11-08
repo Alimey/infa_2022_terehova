224571e08b3340974651cf976a2cce4e3480cce1
Alimey <Alimey.fly@mail.ru>
2022-11-08 17:22:55 +0300

    Первое задание лабы

---
+++ lab8/N1.py
@@ -0,0 +1,30 @@
+import pygame
+from pygame.draw import *
+
+pygame.init()
+
+FPS = 30
+screen = pygame.display.set_mode((400, 400))
+
+circle(screen, 'yellow', (200, 200), 100, width = 0)
+circle(screen, 'black', (200, 200), 100, width = 1)
+
+circle(screen, 'red', (160, 170), 20, width = 0)
+circle(screen, 'black', (160, 170), 20, width = 1)
+circle(screen, 'black', (160, 170), 8.5, width = 0)
+
+circle(screen, 'red', (250, 170), 15, width = 0)
+circle(screen, 'black', (250, 170), 15, width = 1)
+circle(screen, 'black', (250, 170), 7.5, width = 0)
+
+rect(screen, 'black', (150, 250, 100, 20), width=0)
+
+polygon(screen, 'black', [(80, 112),(84, 104),(190, 150),(186, 160)], width=0)
+polygon(screen, 'black', [(309, 137),(306, 129),(220, 155),(224, 165)], width=0)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.flip()
